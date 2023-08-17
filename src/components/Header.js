bb6e002c4189936faa38a4655981376280a2e0c8
Rahul Kumar <username78588@gmail.com>
2023-08-17 21:20:12 +0530

    ADD:Car Slice Reducer

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
+import { selectCars } from "../features/car/carSlice";
+import { useSelector } from "react-redux";
 
 function Header() {
   const [burgerStatus, setBurgerStatus] = useState(false);
+  const cars = useSelector(selectCars);
   return (
     <Container>
       <a>
@@ -12,10 +15,10 @@ function Header() {
       </a>
 
       <Menu>
-        <a href="#">Model S</a>
-        <a href="#">Model 3</a>
-        <a href="#">Model X</a>
-        <a href="#">Model Y</a>
+        {
+          cars && cars.map((car,index)=>( <a key={index} href="#">{car}</a>))
+        }
+        
       </Menu>
       <RightMenu>
         <a href="#">Shop</a>
@@ -27,6 +30,9 @@ function Header() {
         <CloseWrapper>
           <CustomClose onClick={() => setBurgerStatus(false)} />
         </CloseWrapper>
+        {
+          cars && cars.map((car,index)=>( <li><a key={index} href="#">{car}</a></li>))
+        }
         <li>
           <a>Existing Inventory</a>
         </li>
